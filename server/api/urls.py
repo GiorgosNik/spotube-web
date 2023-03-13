b51457bd6b2a1a9858300d6a28f9de7a98f249bf
AngelPn <aggi.pa@gmail.com>
2023-03-13 12:24:11 +0200

    create requirements and debugger launch

--- server/api/urls.py
+++ server/api/urls.py
@@ -3,4 +3,5 @@ from api import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("hello/<name>", views.hello_there, name="hello_there"),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.timezone import datetime
+import re
 
 # Create your views here.
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return HttpResponse(content)

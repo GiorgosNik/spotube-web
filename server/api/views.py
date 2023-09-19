510932883a1122464a9e8d512a353306adb826f5
Angelina Panagopoulou <aggi.pa@gmail.com>
2023-09-19 22:25:05 +0300

    feature(django): added cancel_download

--- server/api/views.py
+++ server/api/views.py
@@ -37,18 +37,35 @@ class Download(APIView):
         downloaders[session_id] = downloader
         downloader.start_downloader(data['playlist_link'])
         return HttpResponse("Download for user " + session_id + " started!")
+    
+class CancelDownload(APIView):
+    parser_classes = [JSONParser]
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            session_id = data['session_id']
+            downloader = downloaders[session_id]
+            downloader.cancel_downloader()
+            return HttpResponse("Cancel download for user " + session_id + " started!")
+        except Exception as e:
+            return HttpResponse(e, status = 400) 
 
 def get_status(request, session_id):
-    downloader = downloaders[session_id]
-    status_info = {
-        "progress": downloader.get_progress(),
-        "progressPercentage": downloader.get_progress() / downloader.get_total() * 100,
-        "total": downloader.get_total(),
-        "failed": downloader.get_fail_counter(),
-        "succeeded": downloader.get_success_counter(),
-        "ETA": downloader.get_eta(),
-    }
-    return HttpResponse(status_info)
+    try:
+        downloader = downloaders[session_id]
+        status_info = {
+            "progress": downloader.get_progress(),
+            "progressPercentage": downloader.get_progress() / downloader.get_total() * 100,
+            "total": downloader.get_total(),
+            "failed": downloader.get_fail_counter(),
+            "succeeded": downloader.get_success_counter(),
+            "ETA": downloader.get_eta(),
+        }
+        return HttpResponse(status_info)
+    except TypeError:
+        return HttpResponse("Unable to cast to Downloader", status = 400) 
+
 
 def get_songs(request, session_id):
     zip_file = zipfile.ZipFile('songs' + session_id + '.zip', 'w')
@@ -59,6 +76,7 @@ def get_songs(request, session_id):
 
     zip_file_path = './songs' + session_id + '.zip'
     with open(zip_file_path, 'rb') as zip:
+        del downloaders[session_id] # Remove downloader from dictionary
         response = HttpResponse(zip.read())
         response['content_type'] = 'application/zip'
         response['Content-Disposition'] = 'attachment; filename="songs' + session_id + '.zip"'
@@ -68,19 +86,3 @@ def get_songs(request, session_id):
 
 def home(request):
     return HttpResponse("Hello, Django!")
-
-def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return HttpResponse(content)

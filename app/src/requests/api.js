5efc8eb4fd12cd1c9e0d5ec7ab87037336932b07
Giorgos Nikolaou <giorgosnl17@gmail.com>
2023-11-11 16:59:34 +0200

    Update logo

--- app/src/requests/api.js
+++ app/src/requests/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-export function downloadRequest(playlist_link, session_id) {
+export function downloadRequest(playlist_link, session_id, normalize_volume = true) {
   console.log(process.env.REACT_APP_API_BASE_URL);
     const requestOptions = {
         playlist_link: playlist_link,
         session_id: session_id,
+        normalize_volume: normalize_volume
       };
   return axios.post(`${process.env.REACT_APP_API_BASE_URL}/download`,requestOptions);
 }
